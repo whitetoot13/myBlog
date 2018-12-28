7f989b9f27aeaa1cb1916d0b9b92d039c9a43e80
Whitetoot13 <nishantluitel2@gmail.com>
2018-12-27 19:44:46 -0500

    i changed

--- src/main/java/com/myblog/controller/UserController.java
+++ src/main/java/com/myblog/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.myblog.entity.User;
 import com.myblog.service.UserService;
-//just try
+//just try alone
 @RestController
 @RequestMapping("/user")
 
